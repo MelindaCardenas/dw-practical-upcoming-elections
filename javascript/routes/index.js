234ec641cc8227baf8f36e5419a8f74d0d651d89
Melinda Cardenas <mc6377@nyu.edu>
2021-03-22 08:36:33 -0400

    add search route

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var us_states = require('../us_state.js');
+const searchController = require('../controllers/searchController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Find My Election', states: us_states });
 });
+
+router.post('/search', searchController.getUpcomingElections);
 
 module.exports = router;
