2c37ff91624bf0ca22af10d300f499406dea43e3
Melinda Cardenas <mc6377@nyu.edu>
2021-03-22 08:35:51 -0400

    create search model, validates user input

---
+++ javascript/models/SearchUpcomingElections.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+const Search = function(state, place){
+    this.state = state;
+    this.place = place;
+    this.errors = [];
+}
+
+Search.prototype.validateUserInput = function() {
+    if(this.state === "" || this.place === ""){
+        this.errors.push("Please enter a place and state")
+    }
+}
+
+module.exports = Search
