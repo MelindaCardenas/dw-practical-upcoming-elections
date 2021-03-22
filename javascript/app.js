dfdbdfe54e1629752dd40c7d24e9cb8ca5d2e291
Melinda Cardenas <mc6377@nyu.edu>
2021-03-22 08:34:06 -0400

    add search route

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -18,7 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
+app.use('/search', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
